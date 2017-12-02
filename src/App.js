b8555c6a4e2446ecd8d49ac827c75bce364a9ada
Madhav Rathi <mdhvrthi@gmail.com>
2017-12-02 23:05:22 +0530

    added responsiveness

--- src/App.js
+++ src/App.js
@@ -5,22 +5,37 @@ import Work from './components/Work/work.react';
 import About from './components/About/about.react';
 import Projects from './components/Projects/projects.react';
 import { Switch, Route } from 'react-router-dom';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import $ from "jquery";
 import './App.css';
 
 class App extends Component{
 
-    render(){
-      const particleStyle = {
-        position: 'relative',
-        zIndex: -100
-      }
+  componentDidMount() {
+    $('.menu').on('click', function(e){
+       $(this).toggleClass('active');
+       $(this).siblings('.fullscreen-menu').toggleClass('active');
+    });
+  }
 
+    render(){
         return (
           <div>
-            <div className='particles'>
-            </div>
-            <div className='stuff'>
+            <div>
+              <div className="header">
+                <div className="menu-container">
+                  <div className="menu"></div>
+                  <div className="fullscreen-menu">
+                    <ul>
+                      <li><a href='/'>Home</a></li>
+                      <li><a href='/about'>About</a></li>
+                      <li><a href='/work'>Work</a></li>
+                      <li><a href='/projects'>Projects</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+
               <div className='container borderXwidth'>
                 <Link to="/"><a>Home</a></Link>
                 <Link to="/about"><a>About</a></Link>
--- src/components/Intro/intro.react.js
+++ src/components/Intro/intro.react.js
@@ -10,97 +10,40 @@ class intro extends Component {
     document.title = 'Madhav Rathi';
     //  Scrolling to top of page when component loads
     $('html, body').animate({ scrollTop: 0 }, 'fast');
-
-    const contentStyle = {
-      position: 'relative',
-      zIndex: 100,
-    }
-
-var words = ['an open source enthusiast','a ReactJS lover','a full stack developer'];
-var numOfWords = words.length;
-var counter = 0;
-
-setInterval(() => {
-  //make the fing thing rotate
-	$('#spinner').toggleClass('rotate');
-	//
-}, 1750);
-
-setInterval(() => {
-
-	var coreAnim = () => {
-		var live = $('.live');
-		var bottom = $('.bottom');
-
-	   //live moves to top and hide
-	   live.animate({
-			opacity: 0.0,
-			marginTop: "-100px"
-		}, 1000, 'linear', () => {
-			live.removeClass('live').addClass('bottom hidden');
-			live.removeAttr('style');
-		});
-	   //bottom unhide moves to live
-	   bottom.text(words[counter]);
-	   bottom.removeClass("hidden");
-	   bottom.animate({
-			opacity: 1.0,
-			marginTop: "0px"
-		}, 1000, 'linear', () => {
-			bottom.removeClass('bottom').addClass('live');
-			bottom.removeAttr('style');
-		});
-	};
-
-	var updateCounter = () => {
-		if((counter + 1) == numOfWords){
-			counter = 0;
-		} else {
-			counter++;
-		}
-	};
-
-	var cleanUp = () => {
-		var item = $('.bottom');
-		var after = item.next();
-		item.insertAfter(after);
-	};
-	coreAnim();
-	updateCounter();
-	cleanUp();
-
-}, 3500);
-
   }
-
   render(){
 
       return (
         <div className='section'>
-          <img className='my_image' src={madhav} alt='Madhav'></img>
-          <div id="centerMe">
-            <div className="floatLeft">
-              <div className="title">I am Madhav Rathi</div>
-              <div id="spinner" className="title rotate">+</div>
-            </div>
-            <div id="textWrapper">
-              <div className="title live">a full stack developer</div>
-              <div className="title bottom hidden"></div>
-            </div>
-          </div>
+          <center><img className='my_image' src={madhav} alt='Madhav'></img></center>
+
+          <center className="title">
+            Hello, I am <span className='name'> Madhav Rathi</span>
+          </center>
+
           <center>
+            <a target='_blank' href='https://docs.google.com/document/d/1AvT9Gw0DBPGLelz1AIdB0Clor4Jk2MTZLZZDqTGPO2U/edit?usp=sharing'>
+              <button className="btn_r btn-1 btn-1e">RESUME</button>
+            </a>
+          </center>
+
+          <center className='bottom_social'>
             <Button target='_blank' href='https://www.facebook.com/madhav.rathi.9' color='facebook'>
               <Icon name='facebook' /> Facebook
             </Button>
+            {' '}
             <Button target='_blank' href='https://twitter.com/mdhvrthi' color='twitter'>
               <Icon name='twitter' /> Twitter
             </Button>
+            {' '}
             <Button target='_blank' href='https://plus.google.com/u/0/101763201361821080686' color='google plus'>
               <Icon name='google plus' /> Google Plus
             </Button>
+            {' '}
             <Button target='_blank' href='https://www.linkedin.com/in/madhav-rathi-120b57129/' color='linkedin'>
               <Icon name='linkedin' /> LinkedIn
             </Button>
+            {' '}
             <Button target='_blank' href='https://www.instagram.com/madhav_rathi/' color='instagram'>
               <Icon name='instagram' /> Instagram
             </Button>
